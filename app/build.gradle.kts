2dede9117e450d9afe8f0e573794c58dc4e02949
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-05-12 23:28:10 +0530

    phase 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,11 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-ui:1.4.1")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
